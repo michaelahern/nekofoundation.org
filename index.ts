ce0a66db7b21dcbce536692222a80c03a3dd65fc
Michael Ahern <michael.ahern@gmail.com>
2022-12-22 19:16:13 -0700

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { NekoFoundationSite } from './site';
+
+const app = new cdk.App();
+new NekoFoundationSite(app, 'NekoFoundationSiteStack', {});
---
+++ site.ts
@@ -0,0 +1,54 @@
+import * as cdk from 'aws-cdk-lib';
+
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+
+import { Construct } from 'constructs';
+
+export class NekoFoundationSite extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // CloudFront Origin Access Identity
+        const nekoSiteOriginAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'NekoSiteOriginAccessIdentity', {});
+
+        // S3 Bucket: nekofoundation.org
+        const nekoSiteBucket = new s3.Bucket(this, 'NekoSiteBucket', {
+            autoDeleteObjects: true,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            bucketName: 'nekofoundation.org',
+            encryption: s3.BucketEncryption.S3_MANAGED,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        nekoSiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [nekoSiteBucket.arnForObjects('*')],
+            principals: [new iam.CanonicalUserPrincipal(nekoSiteOriginAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+        }));
+
+        new cdk.CfnOutput(this, 'NekoSiteBucketName', { value: nekoSiteBucket.bucketName });
+
+        // CloudFront Distribution: nekofoundation.org
+        const distribution = new cloudfront.Distribution(this, 'NekoSiteDistribution', {
+            defaultBehavior: {
+                origin: new cloudfront_origins.S3Origin(nekoSiteBucket, { originAccessIdentity: nekoSiteOriginAccessIdentity }),
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL
+            },
+            defaultRootObject: "index.html",
+            //domainNames: ['www.nekofoundation.org']
+        });
+
+        new cdk.CfnOutput(this, 'NekoSiteDistributionId', { value: distribution.distributionId });
+
+        new s3deploy.BucketDeployment(this, 'NekoSiteBucketDeployment', {
+            destinationBucket: nekoSiteBucket,
+            sources: [s3deploy.Source.asset('./site-contents')],
+            distribution,
+            distributionPaths: ['/*'],
+        });
+    }
+}
